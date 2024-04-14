1f73042b865c7eebb11f867e8db239a7c7401fec
Akash Singh <akashsingh2210670@gmail.com>
2024-04-14 08:54:19 +0530

    Refactored ID's for blogs, comments and users
    Signed-off-by: Akash Singh <akashsingh2210670@gmail.com>

--- db/post.go
+++ db/post.go
@@ -2,16 +2,23 @@ package db
 
 import (
 	"blog/contracttesting/models"
-	"fmt"
 	"log"
+	"math/rand"
+	"time"
 )
+
+func generateRandomID() int {
+	rand.Seed(time.Now().UnixNano())
+	min := 100000
+	max := 999999
+	return rand.Intn(max-min+1) + min
+}
 
 func CreateBlogForUserID(userID int, Title string, Content string, Subtitle string, Image string) (*models.BlogPost, error) {
 	// Execute SQL query to create blog for user ID
-	query := "INSERT INTO blog_posts (user_id, title, content , subtitle , image)  VALUES ($1, $2, $3 , $4 , $5) RETURNING id, user_id, title, content, created_at , subtitle , image"
-	row := db.QueryRow(query, userID, Title, Content, Subtitle, Image)
+	query := "INSERT INTO blog_posts (id, user_id, title, content, created_at, subtitle, image) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id, user_id, title, content, created_at, subtitle, image"
+	row := db.QueryRow(query, generateRandomID(), userID, Title, Content, time.Now(), Subtitle, Image)
 	blog := &models.BlogPost{}
-	fmt.Println("blog", blog)
 	err := row.Scan(&blog.ID, &blog.UserID, &blog.Title, &blog.Content, &blog.CreatedAt, &blog.Subtitle, &blog.Image)
 	if err != nil {
 		log.Printf("Error scanning blog row: %v\n", err)
@@ -22,8 +29,8 @@ func CreateBlogForUserID(userID int, Title string, Content string, Subtitle stri
 
 func CreateCommentForBlogID(userID int, blogID int, Content string) (*models.Comment, error) {
 	// Execute SQL query to create comment for blog ID
-	query := "INSERT INTO comments (user_id, blog_id, content) VALUES ($1, $2, $3) RETURNING id, user_id, blog_id, content, created_at"
-	row := db.QueryRow(query, userID, blogID, Content)
+	query := "INSERT INTO comments (id, user_id, blog_id, content) VALUES ($1, $2, $3, $4) RETURNING id, user_id, blog_id, content, created_at"
+	row := db.QueryRow(query, generateRandomID(), userID, blogID, Content)
 	comment := &models.Comment{}
 	err := row.Scan(&comment.ID, &comment.UserID, &comment.BlogID, &comment.Content, &comment.CreatedAt)
 	if err != nil {
